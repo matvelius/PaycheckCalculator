f1147acecd96a6e47e4a12e679621711be43be2b
student7 <student7@student7s-macbook-air.local>
2019-02-07 20:04:58 -0600

    Initial commit - start with basic functionality of imputs employee ID, hours, and rates to calculate total

--- PaycheckCalculator/ViewController.swift
+++ PaycheckCalculator/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
+//var hours: Double = 0
+//var rate: Double = 0
+
+//Create a dictionary with list of employee IDs and respective names
+var employees = [123: "Richard", 124: "Luke", 125: "Cheryl"]
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var employeeID: UITextField!
+    
+    @IBOutlet weak var hoursWorked: UITextField!
+    
+    @IBOutlet weak var hourlyRate: UITextField!
+    
+    @IBOutlet weak var showTotal: UILabel!
+    
+    @IBAction func calcButton(_ sender: UIButton) {
+        let hours = Int(hoursWorked.text!)
+        let rate = Int(hourlyRate.text!)
+        let total = hours! * rate!
+        showTotal.text = String(total)
+        //var total = hours * rate
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    
 }
+
 
