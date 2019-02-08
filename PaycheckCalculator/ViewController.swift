8962dc53181048909d1b36990cbad037dedbbadc
student7 <student7@student7s-macbook-air.local>
2019-02-07 19:24:07 -0600

    Initial Commit

---
+++ PaycheckCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaycheckCalculator
+//
+//  Created by student7 on 2/7/19.
+//  Copyright © 2019 student7. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
